67adf76aff5914e26fe571e0f2bdf089ba3c23c1
Oakley <dalford278@gmail.com>
2026-01-10 18:10:21 -0500

    0.0.13 updated configs for Docker

--- configs/config.go
+++ configs/config.go
@@ -1,41 +1,45 @@
 package configs
 
 import (
-	"log"
+	"os"
 
-	"github.com/spf13/viper"
+	"gopkg.in/yaml.v3"
 )
 
+type DatabaseConfig struct {
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	Name     string `yaml:"name"`
+}
+
 type Config struct {
-	Server   ServerConfig
+	Server struct {
+		Port int `yaml:"port"`
+	}
 	Database DatabaseConfig
 }
 
-type ServerConfig struct {
-	Port int
-}
-
-type DatabaseConfig struct {
-	Host     string
-	Port     int
-	User     string
-	Password string
-	Name     string
-}
-
 func LoadConfig() *Config {
-	viper.SetConfigName("config")
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath("./configs")
+	cfg := &Config{}
 
-	if err := viper.ReadInConfig(); err != nil {
-		log.Fatalf("error reading config file: %v", err)
+	file, err := os.ReadFile("configs/config.yaml")
+	if err == nil {
+		_ = yaml.Unmarshal(file, cfg)
+	}
+	if v := os.Getenv("DB_HOST"); v != "" {
+		cfg.Database.Host = v
+	}
+	if v := os.Getenv("DB_USER"); v != "" {
+		cfg.Database.User = v
+	}
+	if v := os.Getenv("DB_PASSWORD"); v != "" {
+		cfg.Database.Password = v
+	}
+	if v := os.Getenv("DB_NAME"); v != "" {
+		cfg.Database.Name = v
 	}
 
-	var cfg Config
-	if err := viper.Unmarshal(&cfg); err != nil {
-		log.Fatalf("unable to decode config: %v", err)
-	}
-
-	return &cfg
+	return cfg
 }
--- pkg/db/connection.go
+++ pkg/db/connection.go
@@ -3,19 +3,24 @@ package db
 import (
 	"InventoryManagement/configs"
 	"fmt"
-	"log"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func Connect(cfg configs.DatabaseConfig) *gorm.DB {
-	dsn := "host=localhost user=postgres password=postgres dbname=inventory port=5432 sslmode=disable"
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%d sslmode=disable",
+		cfg.Host,
+		cfg.User,
+		cfg.Password,
+		cfg.Name,
+		cfg.Port,
+	)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("failed to connect to database")
+		panic(err)
 	}
-	fmt.Println("📦 Database connected")
 	return db
 }
